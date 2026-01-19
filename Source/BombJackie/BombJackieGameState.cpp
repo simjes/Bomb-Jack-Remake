f9ea3f5672ba113a8e788f1b3dc0b6990f442ff5
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2026-01-19 22:46:45 +0100

    create BombJackie game state for timer and score

---
+++ Source/BombJackie/BombJackieGameState.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BombJackieGameState.h"
+
+
+#include "TimerManager.h"
+
+void ABombJackieGameState::BeginPlay()
+{
+	Super::BeginPlay();
+	CurrentGameState = EGameState::Playing;
+	GetWorldTimerManager().SetTimer(CountdownTimerHandle, this, &ABombJackieGameState::CountDown, 1.0f, true);
+}
+
+void ABombJackieGameState::CountDown()
+{
+	ABombJackieGameState::HandleDecreaseTime(1);
+}
+
+
+void ABombJackieGameState::HandleIncreaseScore(const int Points)
+{
+	Score += Points;
+
+	OnIncreaseScore.ExecuteIfBound(Score);
+}
+
+void ABombJackieGameState::HandleDecreaseTime(const int Seconds)
+{
+	TimeLeftSeconds -= Seconds;
+	TimeLeftSeconds = std::max(TimeLeftSeconds, 0);
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::White,
+		                                 FString::Printf(TEXT("Time left: %d"), TimeLeftSeconds));
+	}
+
+	OnCountDown.ExecuteIfBound(TimeLeftSeconds);
+
+	if (TimeLeftSeconds == 0)
+	{
+		CurrentGameState = EGameState::GameOver;
+		GetWorldTimerManager().ClearTimer(CountdownTimerHandle);
+		// TODO: send game over event
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::White, TEXT("Game Over"));
+	}
+}
---
+++ Source/BombJackie/EGameState.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+UENUM(BlueprintType)
+enum class EGameState : uint8
+{
+	Playing = 0 UMETA(DisplayName = "Playing"),
+	GameOver = 1 UMETA(DisplayName = "GameOver")
+};
