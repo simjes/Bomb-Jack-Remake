3348ab989d62ec9ee7cf98bdadd10343fe0942ca
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2026-03-19 11:07:23 +0100

    Remove unused things (#107)
    * cleaned up c++ code
    
    * remove lots of unused stuff
    
    * remove third person level

--- Source/BombJackie/EGameState.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-
-UENUM(BlueprintType)
-enum class EGameState : uint8
-{
-	Playing = 0 UMETA(DisplayName = "Playing"),
-	GameOver = 1 UMETA(DisplayName = "GameOver"),
-	Victory = 2 UMETA(DisplayName = "Victory")
-};
