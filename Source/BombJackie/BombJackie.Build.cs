79e05389810b0fd0e16aef9b6d06119a2b231168
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2026-04-14 22:25:13 +0200

    add bomb lit vfx, delete redirectors, adjust lit fuse sound

--- Source/BombJackie/BombJackie.Build.cs
+++ Source/BombJackie/BombJackie.Build.cs
@@ -4,50 +4,39 @@ using UnrealBuildTool;
 
 public class BombJackie : ModuleRules
 {
-	public BombJackie(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public BombJackie(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] {
-			"Core",
-			"CoreUObject",
-			"Engine",
-			"InputCore",
-			"EnhancedInput",
-			"AIModule",
-			"StateTreeModule",
-			"GameplayStateTreeModule",
-			"UMG",
-			"Slate",
+        PublicDependencyModuleNames.AddRange(new[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput",
+            "AIModule",
+            "StateTreeModule",
+            "GameplayStateTreeModule",
+            "UMG",
+            "Slate",
             "NavigationSystem",
             "Niagara"
-		});
+        });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PrivateDependencyModuleNames.AddRange(new string[] { });
 
-		PublicIncludePaths.AddRange(new string[] {
-			"BombJackie",
-			"BombJackie/Variant_Platforming",
-			"BombJackie/Variant_Platforming/Animation",
-			"BombJackie/Variant_Combat",
-			"BombJackie/Variant_Combat/AI",
-			"BombJackie/Variant_Combat/Animation",
-			"BombJackie/Variant_Combat/Gameplay",
-			"BombJackie/Variant_Combat/Interfaces",
-			"BombJackie/Variant_Combat/UI",
-			"BombJackie/Variant_SideScrolling",
-			"BombJackie/Variant_SideScrolling/AI",
-			"BombJackie/Variant_SideScrolling/Gameplay",
-			"BombJackie/Variant_SideScrolling/Interfaces",
-			"BombJackie/Variant_SideScrolling/UI"
-		});
+        PublicIncludePaths.AddRange(new[]
+        {
+            "BombJackie"
+        });
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
 }
